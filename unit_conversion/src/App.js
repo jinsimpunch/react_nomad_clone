c55e3f20437b9fec0df282341ff33ffa22930f4c
indoorKeyman <119554062+indoorKeyman@users.noreply.github.com>
2023-01-23 00:43:46 +0900

    make selection

--- unit_conversion/src/App.js
+++ unit_conversion/src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 
-
-function App() {
-
+function MinutesToHours() {
   const [amount, setAmount] = React.useState()
   const [flipped, setFlipped] = React.useState(false)
 
@@ -20,10 +18,8 @@ function App() {
     setFlipped((current) => !current)
   }
 
-
   return (
     <div>
-      <h1> Hi! Super Converter</h1>
       <label htmlFor="amount">minutes</label>
       <input disabled={flipped} onChange={onChange} value={flipped ? amount * 60 : amount} id="amount" placeholder="amount" type="number" />
       <h2>you enter this {amount} amount!</h2>
@@ -32,6 +28,36 @@ function App() {
       <br />
       <button onClick={reset}>reset</button>
       <button onClick={onFlip}>{flipped ? "Turn back" : "Invert"}</button>
+    </div>
+  );
+
+
+}
+
+function KmToMiles() {
+  return <h3>KM 2 M</h3>
+}
+
+function App() {
+
+  const [index, setIndex] = React.useState(0)
+  const onSelect = (e) => {
+    setIndex(e.target.value)
+    console.log(index)
+  }
+
+  return (
+    <div>
+      <h1> Hi! Super Converter</h1>
+      <select value={index} onChange={onSelect}>
+        <option value="0">select</option>
+        <option value="1"> Minutes & Hours</option>
+        <option value="2">KM & Miles</option>
+      </select>
+      <hr />
+      {index === 0 ? "Please select unit!" : null}
+      {index === 1 ? <MinutesToHours /> : null}
+      {index === 2 ? <KmToMiles /> : null}
     </div>
   );
 }
