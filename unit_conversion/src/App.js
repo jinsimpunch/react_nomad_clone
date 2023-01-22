7af62c52a923bf02130f5901981b5b74111f2967
indoorKeyman <119554062+indoorKeyman@users.noreply.github.com>
2023-01-22 19:49:19 +0900

    unit conversion app

--- unit_conversion/src/App.js
+++ unit_conversion/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
 
 function App() {
+
+  const [amount, setAmount] = React.useState()
+  const [flipped, setFlipped] = React.useState(false)
+
+  const onChange = (e) => {
+    setAmount(e.target.value)
+    console.log(e.target.value)
+  }
+
+  const reset = () => {
+    setAmount(0);
+  }
+
+  const onFlip = () => {
+    reset()
+    setFlipped((current) => !current)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Hi! Super Converter</h1>
+      <label htmlFor="amount">minutes</label>
+      <input disabled={flipped} onChange={onChange} value={flipped ? amount * 60 : amount} id="amount" placeholder="amount" type="number" />
+      <h2>you enter this {amount} amount!</h2>
+      <label htmlFor="hours">Hours</label>
+      <input onChange={onChange} disabled={!flipped} value={flipped ? amount : amount / 60} id="hours" placeholder="Hours" type="number" />
+      <br />
+      <button onClick={reset}>reset</button>
+      <button onClick={onFlip}>{flipped ? "Turn back" : "Invert"}</button>
     </div>
   );
 }
