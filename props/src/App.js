071f9f00cb8d93343df83f1889f24e74f0f13df7
indoorKeyman <119554062+indoorKeyman@users.noreply.github.com>
2023-01-23 19:08:44 +0900

    how to use props

--- props/src/App.js
+++ props/src/App.js
@@ -1,24 +1,39 @@
+import React from "react";
 
 
-function Btn({ banana }) {
+function Btn({ text, onClick, fontSize = 10 }) {
 
-  return <button style={{
-    backgroundColor: "tomato",
-    color: "white",
-    padding: "10px 20px",
-    border: 0,
-    borderRadius: 10,
-  }}
-  >{banana}</button>
+  console.log(text, "was rendered")
+
+  return <button
+    onClick={onClick}
+    style={{
+      backgroundColor: "tomato",
+      color: "white",
+      padding: "10px 20px",
+      border: 0,
+      borderRadius: 10,
+      fontSize,
+    }}
+  >{text}</button>
 }
 
+const MemorizedBtn = React.memo(Btn)
 
+// Btn.propTypes = {
+//   text: PropTypes.string,
+//   fontSize: PropTypes.number,
+// }
 
 function App() {
+
+  const [value, setValue] = React.useState("Save changes");
+  const changeValue = () => setValue("Rever Changes")
+
   return (
     <div>
-      <Btn banana="Save Changes" />
-      <Btn banana="Continue" />
+      <MemorizedBtn text={value} onClick={changeValue} fontSize={60} />
+      <MemorizedBtn text="Continue" />
     </div>
   );
 }
